3f411c763c70612d507fc15e98133cecdb62d00c
thanhhoa <thanhhoa@localhost>
2023-03-28 22:48:18 +0700

    Update Topic_11 with Custom_Checkbox_Radio_Button

--- src/webdriver/Topic_11_Button_Radio_Checkbox.java
+++ src/webdriver/Topic_11_Button_Radio_Checkbox.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,6 +17,7 @@ import org.testng.annotations.Test;
 
 public class Topic_11_Button_Radio_Checkbox {
 	WebDriver driver;
+	JavascriptExecutor jsExecutor;
 	String projectPath = System.getProperty("user.dir"); //đường dẫn đến thư mục Project
 	String osName = System.getProperty("os.name");
 
@@ -28,6 +30,8 @@ public class Topic_11_Button_Radio_Checkbox {
 		}
 
 		driver = new FirefoxDriver();
+		
+		jsExecutor = (JavascriptExecutor) driver; // luôn luôn tạo sau biến driver của Browser 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
@@ -95,15 +99,29 @@ public class Topic_11_Button_Radio_Checkbox {
 		
 	}
 	@Test
-	public void TC_04_() {
+	public void TC_04_Custom_Checkbox_Radio() {
+		//CASE1: Thẻ input bị che nên ko thao tác được nhưng dùng để verify được vì hàm isSelected() chỉ work với thẻ input
+	
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
+		driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/parent::label")).click();
 		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input")).isSelected());
+
 	}
 	@Test
-	public void TC_05_() {
-		
+	public void TC_05_Custom_Checkbox_Radio() {
+		//CASE4: Dùng JavascriptExcutor để thao tác vào thẻ input đang bị ẩn
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");		
+		jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input")));
+//		jsExecutor.executeScript("arguments[0].click(); arguments[1].sendKeys();", driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input")), "Doi so 2");
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input")).isSelected());
+	
 	}
 	@Test
-	public void TC_06_() {
+	public void TC_06_Custom_Checkbox_Radio() {
+		//check thay doi sau khi select => dung isDisplayed kiem tra attribute da da thay doi do
+		
 		
 	}
 	public void checkToCheckbox(By by) {
